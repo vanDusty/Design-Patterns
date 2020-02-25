70be130386d64b8306195fe70802d1a4f4139cbb
Van <1251787298@qq.com>
2020-02-25 22:53:19 +0800

    if-else 优化 init

---
+++ strategy-factory-single-demo/src/test/java/cn/van/sfs/demo/DemoTest.java
@@ -0,0 +1,26 @@
+package cn.van.sfs.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Copyright (C), 2015-2020, 风尘博客
+ * 公众号 : 风尘博客
+ * FileName: StrategyTest
+ *
+ * @author: Van
+ * Date:     2020-02-14 14:44
+ * Description: ${DESCRIPTION}
+ * Version： V1.0
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class DemoTest {
+
+    @Test
+    public void ddd() {
+        System.out.println(2222);
+    }
+}
